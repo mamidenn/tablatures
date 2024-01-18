20c71f4a96bc615c736f0d8799a1b12cd6794f14
Martin Dennhardt <martin@dennhardt.de>
2024-01-18 11:11:34 +0000

    fix file upload

--- src/library/utils/utils.ts
+++ src/library/utils/utils.ts
@@ -16,23 +16,13 @@ export function extract(content: string, start_token: string, end_token: string)
 }
 
 export function arrayBufferToBase64(buffer: ArrayBuffer) {
-	let binary = '';
-	const bytes = new Uint8Array(buffer);
-	const len = bytes.byteLength;
-	for (let i = 0; i < len; i++) {
-		binary += String.fromCharCode(bytes[i]);
-	}
-	return btoa(binary);
+	const str = String.fromCodePoint(...new Uint8Array(buffer));
+	return btoa(str);
 }
 
 export function base64ToArrayBuffer(base64: string) {
-	const binary_string = window.atob(base64);
-	const len = binary_string.length;
-	const bytes = new Uint8Array(len);
-	for (let i = 0; i < len; i++) {
-		bytes[i] = binary_string.charCodeAt(i);
-	}
-	return bytes.buffer;
+	const bin = atob(base64);
+	return Uint8Array.from(bin, (c) => c.codePointAt(0)!).buffer;
 }
 
 export const GuitarProTab = {
